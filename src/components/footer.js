ad2e46666b9878dcaad79603b53895ac7d5fc85d
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2023-03-14 20:19:14 +0600

    Added About Us Page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,114 @@
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <MDBFooter
+      bgColor="dark"
+      className="text-center text-lg-start text-muted h-100"
+    >
+      <section className="h-auto">
+        <MDBContainer className="text-center text-md-start ">
+          <MDBRow className="pt-4">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4 text-light">
+                <MDBIcon icon="gem" className="me-3" />
+                About US
+              </h6>
+              <p>
+                Few would argue that, despite the advanc ements off eminism over
+                the past three decades, women still face a double standard when
+                it comes to their behavior. While men’s borderline-inappropriate
+                behavior. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </p>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4 text-light">
+                Useful links
+              </h6>
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Home</a>
+                </Link>
+              </p>
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Service</a>
+                </Link>
+              </p>
+              <p>
+                <Link href="/about" legacyBehavior>
+                  <a className="text-reset">About</a>
+                </Link>
+              </p>
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Case study</a>
+                </Link>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4 mt-5">
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Pricing</a>
+                </Link>
+              </p>
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Team</a>
+                </Link>
+              </p>
+              <p>
+                <Link href="/" legacyBehavior>
+                  <a className="text-reset">Blog</a>
+                </Link>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4 text-light">
+                Contact US
+              </h6>
+              <p className="text-light">
+                <MDBIcon icon="home" className="me-2 text-light" />
+                New York, NY 10012, US
+              </p>
+              <p className="text-light">
+                <MDBIcon icon="envelope" className="me-3 text-light" />
+                info@example.com
+              </p>
+              <p className="text-light">
+                <MDBIcon icon="phone" className="me-3 text-light" /> + 01 234
+                567 88
+              </p>
+              <p className="text-light">
+                <MDBIcon icon="print" className="me-3 text-light" /> + 01 234
+                567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div
+        className="text-center p-4 text-light"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2023 Copyright:
+        <a className="text-reset fw-bold text-light"></a>
+      </div>
+    </MDBFooter>
+  );
 };
 
 export default Footer;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,111 @@
+import Link from "next/link";
+import about from "../../styles/about.module.scss";
+import variables from "../../styles/variables.module.scss";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      <div className={variables.banner}>
+        <div className={about.text}>
+          <div className={about.aboutContent}>
+            <h1 className={about.aboutText}>About Us</h1>
+            <p className={about.aboutLinks}>
+              <Link href="/" legacyBehavior>
+                <a className={about.home}>Home</a>
+              </Link>
+              <ArrowForwardIcon className={about.icon} />
+              <Link href="/" legacyBehavior>
+                <a className={about.about}>About</a>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <section className={about.videoArea}>
+        <div className={about.contain}>
+          <div className={about.photoArea}>
+            <div className={about.padd}>
+              <div className={about.picture}>
+                <div className={about.overplay}></div>
+                <Link
+                  href="https://www.youtube.com/watch?v=ARA0AxrnHdM"
+                  legacyBehavior
+                >
+                  <a className={about.playBtn}></a>
+                </Link>
+              </div>
+              <div className={about.desc}>
+                <h4 className={about.desc1}>
+                  Watch this video how they live here
+                </h4>
+                <p className={about.desc2}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={about.fav}>
+        <div className={about.row}>
+          <div className={about.favPet}>
+            <h2 className={about.select}>Please select your favourite pet</h2>
+          </div>
+          <div className={about.fillBtn}>
+            <button className={about.yellowBtn}>Fill Adoption Form</button>
+          </div>
+        </div>
+      </section>
+      <section className={about.homeAbout}>
+        <div className={about.fluid}>
+          <div className={about.all}>
+            <div className={about.leftPage}>
+              <img
+                src="https://preview.colorlib.com/theme/animalshelter/img/about-img.jpg.webp"
+                alt=""
+                className={about.img}
+              />
+            </div>
+            <div className={about.rightPage}>
+              <h1 className={about.globally}>
+                Globally Connected by Large Network
+              </h1>
+              <h5 className={about.ash}>
+                We are here to listen from you deliver exellence
+              </h5>
+              <p className={about.paragh}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+                enim ad minim. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit, sed do eiusmod tempor.
+              </p>
+              <button className={about.detailsBtn}>get details</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={about.valonters}>
+        <div className={about.overplayBg}></div>
+        <div className={about.cont}>
+          <div className={about.rove}>
+            <h1 className={about.help}>Want to help? Become a Volunteer</h1>
+            <p className={about.lorem}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit, sed do eiusmod tempor.
+            </p>
+            <div className={about.Btn}>
+              <button className={about.pdf}>View pdf details</button>
+              <button className={about.register}>Register now</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
