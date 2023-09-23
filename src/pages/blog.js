ce7fb9396ffdb670f4c440c374b1cc7adc3e2a42
MrPrezAtambaev <akyl182003@gmail.com>
2023-09-23 11:38:57 +0600

    fixed blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import about from "../../styles/about.module.scss";
-import home from "../../styles/homepage.module.scss";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-
-export default function Blog() {
-  return (
-    <div className={home.banner}>
-      <div className={about.text}>
-        <div className={about.aboutContent}>
-          <h1 className={about.aboutText}>Blog</h1>
-          <p className={about.aboutLinks}>
-            <Link href="/" legacyBehavior>
-              <a className={about.home}>Home</a>
-            </Link>
-            <ArrowForwardIcon className={about.icon} />
-            <Link href="/blog" legacyBehavior>
-              <a className={about.about}>Blog</a>
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
-=======
 import Link from "next/link";
 import about from "../../styles/about.module.scss";
 import home from "../../styles/homepage.module.scss";
@@ -51,4 +24,3 @@ export default function Blog() {
     </div>
   );
 }
->>>>>>> samat
